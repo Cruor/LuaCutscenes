61995739a9a860d205b61b0270c706242c11efe1
Cruor <cruoratmc@gmail.com>
2020-10-30 14:36:46 +0100

    Updated documentation Fixed typo in talker example Added onEnd documentation in talker example

--- LuaCutscenes/Assets/LuaCutscenes/examples/example_talker.lua
+++ LuaCutscenes/Assets/LuaCutscenes/examples/example_talker.lua
@@ -3,7 +3,7 @@
 -- Leave callbacks global to automatically detect it on c# side.
 -- This assumes you know your way around Lua, if you don't then <a href=https://www.lua.org/pil/5.1.html>please consider reading the Lua PIL</a>.
 -- <h2>Setting up in map editor</h2>
--- In the case of this talker, the filename for the entity field is "Assets/Luatalkers/example_talker".
+-- In the case of this talker, the filename for the entity field is "Assets/LuaCutscenes/example_talker".
 -- Please use your own folders for your own talkers, like any other Celeste asset collisions can happen.<br>
 -- <h2>Using C# in talker</h2>
 -- Any imports from C# can be done with prefixing # in the require string. For example `local celeste = require("#Celeste")`.<br>
@@ -20,4 +20,17 @@ function onTalk()
     wait(2)
 
     enableMovement()
+end
+
+--- Callback for when the cutscene ends.
+-- Function, no yielding actions allowed.
+-- That means no walking, waiting etc.
+-- Only "clean up" actions.
+-- @tparam #Celeste.Level room Current room.
+-- @bool wasSkipped If the cutscene was skipped.
+function onEnd(room, wasSkipped)
+    -- Skipping cutscenes is rude, you know :/
+    if wasSkipped then
+        die()
+    end
 end
